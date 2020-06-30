7c471200514bbba1430f3f7330897197d33baec1
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-06-30 22:53:30 +0200

    default layout set to fibonacci

--- config.def.h
+++ config.def.h
@@ -44,11 +44,10 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 #include "layouts.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "[@]",      spiral },		/* fibonacci */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-
-	{ "[@]",      spiral },		/* fibonacci */
 	{ "[\\]",     dwindle },	/* like fibo but towards center */
 };
 
@@ -102,10 +101,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,  			XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,			XK_r,	   setlayout,	   {.v = &layouts[3]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,			XK_r,	   setlayout,	   {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,		XK_r,	   setlayout,	   {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
