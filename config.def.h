e83c50739fc702a7db492287a3eb7e37b8a6d017
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-07-08 08:49:09 +0200

    changed font and added newsboat binding

--- config.def.h
+++ config.def.h
@@ -9,14 +9,14 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 14;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */ 
 static const int topbar             = 1;        /* 0 means bottom bar */ 
-static const char *fonts[]          = { "monospace:size=11","FontAwesome:pixelsize=12:antialias=true:autohint=true" };
+static const char *fonts[]          = { "firamono:size=9","FontAwesome:pixelsize=12:antialias=true:autohint=true" };
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const char dmenufont[]       = "monospace:size=11";
+static const char dmenufont[]       = "firamono:size=9";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -88,6 +88,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,		        		XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,						XK_n,	   spawn,	       SHCMD("$TERMINAL -e ranger") },
+    { MODKEY,                       XK_v,      spawn,          SHCMD("$TERMINAL -e newsboat") },
 	{ MODKEY,						XK_b,	   spawn,	       SHCMD("$BROWSER") },
 	{ MODKEY,						XK_a,	   spawn,	       SHCMD("$TERMINAL -e htop") },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
