c36c01061ed1d2e2de076b3078fbe7c1e5dc123d
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-10-03 15:16:44 +0200

    adjustments to gaps, font size and dmenu placement

--- config.def.h
+++ config.def.h
@@ -7,16 +7,16 @@ static const unsigned int systraypinning = 0;	/* 0: sloppy systray follows selec
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 6;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */ 
-static const int topbar             = 1;        /* 0 means bottom bar */ 
-static const char *fonts[]          = { "monospace:size=9","FontAwesome:pixelsize=12:antialias=true:autohint=true" };
+static const int topbar             = 0;        /* 0 means bottom bar */ 
+static const char *fonts[]          = { "monospace:size=10","FontAwesome:pixelsize=12:antialias=true:autohint=true" };
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const char dmenufont[]       = "monospace:size=9";
+static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -80,7 +80,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
